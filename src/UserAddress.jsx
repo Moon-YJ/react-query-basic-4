0587f6ba9d5e0641049902b7b221f0651058acfe
Moon-YJ <tunjeong@gmail.com>
2023-12-26 13:24:58 +0900

    4버전대 적용

--- src/UserAddress.jsx
+++ src/UserAddress.jsx
@@ -1,13 +1,14 @@
 import { useUserQuery } from "./hooks/useUserQuery";
 
 export default function UserAddress() {
-  const { data, isSuccess, isError } = useUserQuery();
+  const result = useUserQuery(2);
+  console.log(result);
 
   return (
     <div className="UserAddress">
       <h1>User Address</h1>
-      {isSuccess && <p>{data.address.street}</p>}
-      {isError && <p>데이터 반환 실패</p>}
+      {/* {isSuccess && <p>{data.address.street}</p>}
+      {isError && <p>데이터 반환 실패</p>} */}
     </div>
   );
 }
--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { useUserQuery } from "./hooks/useUserQuery";
 
 export default function UserName() {
-  const { data, isSuccess, isError } = useUserQuery();
+  const { data, isSuccess, isError } = useUserQuery(1);
+  const [Num, setNum] = useState(0);
+
   return (
     <div className="UserName">
       <h1>User Name</h1>
+      <p>{Num}</p>
+      <button onClick={() => setNum(Num + 1)}>Plus</button>
       {isSuccess && <p>{data.name}</p>}
       {isError && <p>데이터 반환 실패</p>}
     </div>
