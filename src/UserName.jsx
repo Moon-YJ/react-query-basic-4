6f238f1285b6a2b0b6fa691271b481f8c7d3cf97
Moon-YJ <tunjeong@gmail.com>
2023-12-27 10:42:44 +0900

    목록 출력 기능으로 컴포넌트 구조 변경

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
 import { useUserQuery } from "./hooks/useUserQuery";
 
 export default function UserName() {
-  const [Num, setNum] = useState(1);
-  const { data, isSuccess, isError } = useUserQuery(Num);
+  const { data, isSuccess } = useUserQuery();
 
   return (
     <div className="UserName">
       <h1>User Name</h1>
-      <p>{Num}</p>
-      <button onClick={() => setNum(1)}>변경1</button>
-      <button onClick={() => setNum(2)}>변경2</button>
-      <h2>name1: {isSuccess && data.name}</h2>
-      {isError && <p>데이터 반환 실패</p>}
+      <ul>
+        {isSuccess ? (
+          data.map((user) => <li key={user.id}>{user.name}</li>)
+        ) : (
+          <p>Loading...</p>
+        )}
+      </ul>
     </div>
   );
 }
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -1,21 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 
 const fetchUser = async ({ queryKey }) => {
-  console.log(queryKey);
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${queryKey[1]}`
-  );
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
   return await response.json();
 };
 
+// 데이터 목록 호출 custom hook
 export const useUserQuery = (num) => {
   // useQuery 첫번째 인수로 넣는 값은 무조건 fetching함수로 전달됨
   // useQuery에는 query Key가 동일하면 같은 데이터라고 인지하기 때문에 refetching 처리하지않음
-  return useQuery(["users", num], fetchUser, {
+  return useQuery(["users"], fetchUser, {
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     cacheTime: 1000 * 20, // inactive된 상태에서 얼마동안 데이터를 캐시에 유지시킬지에 대한 시간 정보, 해당 데이터를 컴포넌트에서 활용하지 않더라도 얼마동안 garbage collector에 의한 데이터 삭제를 막을지에 대한 시간값 설정(default 값 5분)
-    staleTime: 1000 * 5, // 서버 데이터를 fetching한 순간부터 언제까지 최신 데이터로 인지시키면서 cacheTime을 소진시키지 않을지에 대한 시간값, 얼마동안 refetching을 하지 않을지에 대한 시간값 설정 (staleTime -> cacheTime) (default 값 0)
+    staleTime: 1000 * 10, // 서버 데이터를 fetching한 순간부터 언제까지 최신 데이터로 인지시키면서 cacheTime을 소진시키지 않을지에 대한 시간값, 얼마동안 refetching을 하지 않을지에 대한 시간값 설정 (staleTime -> cacheTime) (default 값 0)
   });
 };
 
