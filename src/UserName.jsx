49474cc412409e60dc2cdefc9a21bd326e6941a9
Moon-YJ <tunjeong@gmail.com>
2023-12-27 11:59:56 +0900

    mutation을 통해 기존 데이터 수정

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,18 +1,48 @@
-import { useUserQuery } from "./hooks/useUserQuery";
+import { useState } from "react";
+import {
+  //useDeleteQuery,
+  useUpdateUser,
+  useUserQuery,
+} from "./hooks/useUserQuery";
 
 export default function UserName() {
-  const { data, isSuccess } = useUserQuery();
+  const { data, isSuccess } = useUserQuery(2);
+  const [UserName, setUserName] = useState("");
+
+  // const deleteUser = useDeleteQuery();
+  const updateUser = useUpdateUser();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateUser.mutate([UserName, 2]);
+  };
 
   return (
     <div className="UserName">
       <h1>User Name</h1>
-      <ul>
+      {/* <button
+        onClick={() => {
+          deleteUser.mutate(2);
+          //console.log(deleteUser);
+        }}
+      >
+        유저 데이터 삭제
+      </button> */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={UserName || ""}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </form>
+      <p>{isSuccess && data.name}</p>
+      {/* <ul>
         {isSuccess ? (
           data.map((user) => <li key={user.id}>{user.name}</li>)
         ) : (
           <p>Loading...</p>
         )}
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -1,7 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const fetchUser = async ({ queryKey }) => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${queryKey[1]}`
+  );
   return await response.json();
 };
 
@@ -9,11 +11,66 @@ const fetchUser = async ({ queryKey }) => {
 export const useUserQuery = (num) => {
   // useQuery 첫번째 인수로 넣는 값은 무조건 fetching함수로 전달됨
   // useQuery에는 query Key가 동일하면 같은 데이터라고 인지하기 때문에 refetching 처리하지않음
-  return useQuery(["users"], fetchUser, {
+  return useQuery(["users", num], fetchUser, {
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     cacheTime: 1000 * 20, // inactive된 상태에서 얼마동안 데이터를 캐시에 유지시킬지에 대한 시간 정보, 해당 데이터를 컴포넌트에서 활용하지 않더라도 얼마동안 garbage collector에 의한 데이터 삭제를 막을지에 대한 시간값 설정(default 값 5분)
     staleTime: 1000 * 10, // 서버 데이터를 fetching한 순간부터 언제까지 최신 데이터로 인지시키면서 cacheTime을 소진시키지 않을지에 대한 시간값, 얼마동안 refetching을 하지 않을지에 대한 시간값 설정 (staleTime -> cacheTime) (default 값 0)
+  });
+};
+
+// 기존 데이터 삭제하는 함수 (typicode 가이드 문서 참고)
+const deleteUser = async (num) => {
+  const reponse = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${num}`,
+    {
+      method: "DELETE",
+    }
+  );
+  return await reponse.json();
+};
+
+const updateUser = async ([userName, num]) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${num}`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({
+        name: userName,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  const result = await response.json();
+  console.log(result);
+  return result;
+};
+
+// 인수로 순번을 받아서 해당 순번의 데이터를 삭제하는 custom hook
+export const useDeleteQuery = () => {
+  // 기존 App.js에서 생성한 queryClient 인스턴스를 호출하는 함수
+  // 해당 queryClient 인스턴스에서 활용할수 있는 protype method인 setQueryData라는 서버데이터 변경 요청값을 등록하는 함수 가져올 수 있음
+  const queryClient = useQueryClient();
+  // useMutation(비동기 데이터 변경함수, 옵션설정 객체{onSuccess: mutate요청이 성공적으로 수행되면 연결될 핸들러함수})
+  // useMutation 훅이 deleteUser라는 내부 fetching함수를 호출해서 서버데이터 변경 요청
+  return useMutation(deleteUser, {
+    // 서버 데이터 변경 성공시, 변경된 서버 데이터값을 다시 고유의 쿼리키로 등록해서 react-query로 비동기 데이터 관리
+    onSuccess: (args) => {
+      queryClient.setQueryData(["users", args.id], args);
+    },
+  });
+};
+
+// 데이터 변경 커스텀훅
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation(updateUser, {
+    onSuccess: (args) => {
+      console.log(args);
+      queryClient.setQueryData(["users", args.id], args);
+    },
   });
 };
 
